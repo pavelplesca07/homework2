ff23046ae68e1584a7cb65c53b77810324e2915e
pavelplesca <rap2604@mail.ru>
2022-07-31 19:55:33 +0300

    homework

---
+++ homework2/else if & switch/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        if (a > b){
+            System.out.println("a > b");
+        }
+        else if (a < b){
+            System.out.println("a < b");
+        }
+        else {
+            System.out.println("a = b");
+        }
+    }
+}
---
+++ homework2/else if & switch/src/Switch.java
@@ -0,0 +1,23 @@
+public class Switch {
+    public static void main(String[] args) {
+        int e = 4;
+        switch (e) {
+            case 1:
+                System.out.println("Один");
+                break;
+            case 2:
+                System.out.println("Два");
+                break;
+            case 3:
+                System.out.println("Три");
+                break;
+            case 4:
+                System.out.println("Четыре");
+                break;
+            case 5:
+                System.out.println("Пять");
+                break;
+
+        }
+    }
+}
